b55d3a53a56cda77375a6e73664a43425c579db8
Echo-Ether <echo.ether.studio@gmail.com>
2025-06-17 12:19:11 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,15 +1,3 @@
+// Echo & Ether base script
 
-document.addEventListener("DOMContentLoaded", function () {
-  var coll = document.getElementsByClassName("collapsible");
-  for (let i = 0; i < coll.length; i++) {
-    coll[i].addEventListener("click", function () {
-      this.classList.toggle("active");
-      var content = this.nextElementSibling;
-      if (content.style.display === "block") {
-        content.style.display = "none";
-      } else {
-        content.style.display = "block";
-      }
-    });
-  }
-});
+console.log("Welcome to Echo & Ether. Your energy is sacred.");
