f39495dbb36d2e23c3a42abd0462f25a74395415
Echo-Ether <echo.ether.studio@gmail.com>
2025-06-17 10:46:31 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 
-// Placeholder for future interactivity
-console.log("Welcome to Echo & Ether 🌿");
+document.addEventListener("DOMContentLoaded", function () {
+  var coll = document.getElementsByClassName("collapsible");
+  for (let i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function () {
+      this.classList.toggle("active");
+      var content = this.nextElementSibling;
+      if (content.style.display === "block") {
+        content.style.display = "none";
+      } else {
+        content.style.display = "block";
+      }
+    });
+  }
+});
